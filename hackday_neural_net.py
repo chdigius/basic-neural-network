d0ab768145583063f41d013fb6bf5059b7c608f0
Stuart Karp <skarp@wbgames.com>
2019-10-18 14:12:25 -0400

    Sort by price point and only show succesful predictions

--- hackday_neural_net.py
+++ hackday_neural_net.py
@@ -27,11 +27,21 @@ print('Accuracy: %.2f' % (accuracy*100))
 
 # make probability predictions with the model
 predictions = model.predict(X)
-# round predictions 
+# round predictions
 rounded = [round(x[0]) for x in predictions]
 # make class predictions with the model
 predictions = model.predict_classes(X)
 
-# summarize the first 5 cases
-for i in range(10):
-	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], Y[i]))
+# show successful predictions only
+win_data = []
+for i, item in enumerate(X):
+  if predictions[i] == 1 and Y[i] == 1:
+    #print('%s => %d (expected %d)' % (item.tolist(), predictions[i], Y[i]))
+    win_data.append(list(item))
+
+# sort by price
+from operator import itemgetter
+win_data = sorted(win_data, key=itemgetter(5),reverse=True)
+print "User ID, Age, Sex, Num Titles Owned, Product Id, Product Price"
+for i in win_data:
+	print(i)
